6042f0bdf575ba61d28523254142fff9d5641acf
LAPTOP-81QC6VKN\yeico <yeicoarce94@gmail.com>
2020-08-01 21:28:26 -0600

    Clase 4

---
+++ test/pageobjects/navigation.Page.js
@@ -0,0 +1,25 @@
+const Page = require('./page');
+
+/**
+ * sub page containing specific selectors and methods for a specific page
+ */
+class navigationPage extends Page {
+    /**
+     * define selectors using getter methods
+     */
+    get conduitLabel () { return $('a.navbar-brand.ng-binding') }
+    get signInLink () { return $('=Sign in') }
+    get signUpLink () { return $('*=up') }
+
+    goToSignIn() {
+        this.signInLink.click();
+    }
+    getConduitText() {
+        return this.conduitLabel.getText();
+    }
+    open () {
+        return super.open('');
+    }
+}
+
+module.exports = new navigationPage();
